cf5c13ec0a6fca7b9ee20db86696a6aada95528d
Pratiksha khandagale <pratiksha_k@techjoomla.com>
2018-12-31 17:17:22 +0530

    changes

--- src/login.module.ts
+++ src/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { AcademyComponent } from './components/login';
 import { AcademyProvider } from './providers/login';
 import { IonicModule } from 'ionic-angular';
- 
+
 @NgModule({
     imports: [
         // Only if you use elements like ion-content, ion-xyz...
